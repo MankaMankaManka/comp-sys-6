1dc207ec6ec6f00a77207e458cddb1717a00e4f2
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-10-22 23:31:56 +1030

    Update VMTranslator.cpp

--- VMTranslator/VMTranslator.cpp
+++ VMTranslator/VMTranslator.cpp
@@ -37,6 +37,7 @@ string VMTranslator::vm_push(string segment, int offset){
             return "@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
 
         }
+        }
         string segmentBase = "";
         if (segment == "local") {
             segmentBase = "LCL";
